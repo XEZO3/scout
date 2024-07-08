62136590acc9b98d9493ff2aa91b8b698c13b5ba
XEZO3 <ezooooo5.rappelz@gmail.com>
2024-07-08 15:03:49 +0300

    Update ActivityService.php

--- app/Services/LeadersService/ActivityService.php
+++ app/Services/LeadersService/ActivityService.php
@@ -11,11 +11,17 @@ class ActivityService extends BaseService
     public function getAll(){
         $level = auth()->guard('admin')->user()->level;
         if($level!="admin"){
-            $activities['activity'] = activities::with("students")->where("level",$level)->orderBy('created_at')->get();
-        }else{
+            $activities = Activity::where("level", $level)
+            ->with(['students' => function($query) {
+                $query->select('students_id', 'activities_id');
+            }])
+            ->orderBy('created_at')
+            ->get();        }else{
             $activities['activity'] = activities::with("students")->orderBy('created_at')->get();
         }
             $activities['total_user'] = Students::where("level",$level)->count();
+            $activities['activity']['absent_user'] =activities::with("students")->where("level",$level)->orderBy('created_at')->count();
+
         return $this->response(true,"",$activities);
     }
     public function create($data){
